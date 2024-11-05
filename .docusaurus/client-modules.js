017311cfa9856827faf7546c950ff24356b34f70
gwanjae.ji <gwanjae.ji@tigrison.com>
2024-11-05 16:33:31 +0900

    imit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\my-website\\src\\css\\custom.css"),
+];
